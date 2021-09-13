6f77f02a7e2ebe8f5d0932d79137b09ed7321fd0
Kirito <kurulluvarov@gmail.com>
2021-09-13 22:26:27 +0300

    type for usersApi

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -4,15 +4,31 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: `https://social-network.samuraijs.com/api/1.0`,
     headers: {
-        //"API-KEY": "670e02e0-a18b-4af1-b6d8-7bc8edc695ba"
-        "API-KEY": "9796e0df-7156-4180-913f-3e6c30a4c76a"
+        "API-KEY": "670e02e0-a18b-4af1-b6d8-7bc8edc695ba"
+        // "API-KEY": "9796e0df-7156-4180-913f-3e6c30a4c76a"
         
     }
 })
+type UserType = {
+    id: number
+    name: string
+    status: string
+    photos: {
+        small: string
+        large: string
+    }
+    followed: boolean
+}
+
+type GetUsersResponseType = {
+    items: Array<UserType>
+    totalCount: number
+    error: string
+}
 
 export const usersAPI = {
     getUsers(currentPage = 1, pageSize = 5) {
-        return instance.get(`/users?count=${pageSize}&page=${currentPage}`)
+        return instance.get<GetUsersResponseType    >(`/users?count=${pageSize}&page=${currentPage}`)
             .then(response => {
                 return response.data
             })
